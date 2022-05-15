6aadbed6d4e15777e8078b00ff5840890c3b9f73
RyanJeong <wat6988@gmail.com>
2022-05-15 23:14:07 +0900

    Updated on May 15, 2022

--- http_test/README.md
+++ http_test/README.md
@@ -2,6 +2,7 @@
 
 A new Flutter project.
 
-## [HTTP test server accepting GET/POST requests](https://stackoverflow.com/questions/5725430/http-test-server-accepting-get-post-requests)
-* [Post Test Server V2](http://ptsv2.com/)
-> "Here you will find a server which receives any POST you wish to give it and stores the contents for you to review."
+## [httpbin.org]
+A simple HTTP Request & Response Service.
+<b>Run locally:</b> `$ docker run -p 80:80 kennethreitz/httpbin`
+
--- http_test/lib/main.dart
+++ http_test/lib/main.dart
@@ -1,115 +1,113 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+Future<String> send(String pw, String sdk) async {
+  const String url = 'http://192.168.0.10:80/post';
+  final response = await http.post(
+    Uri.parse(url),
+    headers: <String, String>{
+      'ContentType': 'application/json; charset=UTF-8',
+    },
+    body: <String, String> {
+      'password': pw,
+      'sdk_key': sdk
+    },
+  );
+  print("Status code: " + response.statusCode.toString());
+  print("Content length: " + response.contentLength.toString());
+  print("Reason phrase: " + response.reasonPhrase.toString());
+  print(response.body);
 
-  // This widget is the root of your application.
+  return response.body;
+}
+
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _State createState() => _State();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _State extends State<MyApp> {
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController sdkController = TextEditingController();
+  TextEditingController debugController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text('HTTP Test')
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body: Padding(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 200,
+                  color: Color(0xffeeeeee),
+                  padding: EdgeInsets.all(10.0),
+                  child: new ConstrainedBox(
+                    constraints: BoxConstraints(
+                      maxHeight: 200.0,
+                    ),
+                    child: new Scrollbar(
+                      child: new SingleChildScrollView(
+                        scrollDirection: Axis.vertical,
+                        reverse: true,
+                        child: SizedBox(
+                          height: 190.0,
+                          child: new TextField(
+                            maxLines: 100,
+                            controller: debugController,
+                            readOnly: true,
+                            decoration: new InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Debugging area',
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    obscureText: true,
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: sdkController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'SDK Key',
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  child: Text('Submit'),
+                  onPressed: () async {
+                    String res = await send(
+                          passwordController.text, sdkController.text).whenComplete(() => null);
+                    debugController.text = res;
+                  },
+                ),
+              ],
+            )
+        )
     );
   }
 }
