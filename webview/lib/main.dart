33a542139d426d7e28e8da7a0124ceb9bce9e3c0
RyanJeong <wat6988@gmail.com>
2022-06-07 00:49:06 +0900

    fix: Fixed to support iOS

--- webview/lib/main.dart
+++ webview/lib/main.dart
@@ -11,7 +11,7 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const WebView(
-      initialUrl: 'https://www.naver.com',
+      initialUrl: 'https://www.naver.com', // http://10.0.0.1
       javascriptMode: JavascriptMode.unrestricted,
     );
   }
